79a425fb19abdc3b570a3b638b65de2152d84a8c
tgribeauval <36380615+tgribeauval@users.noreply.github.com>
2018-05-29 01:32:42 -0400

    Javascripting

---
+++ objects.js
@@ -0,0 +1,23 @@
+
+// How to console.log pizza.
+
+var pizza = {  
+       toppings: ['cheese', 'sauce', 'pepperoni'],  
+       crust: 'deep dish',  
+       serves: 2  
+     };
+     
+     console.log(pizza)
+     
+    // You can access and manipulate object properties –– the keys and values  
+  that an object contains –– using a method very similar to arrays.  
+   
+  // Here's an example using square brackets:  
+   
+     var example = {  
+       pizza: 'yummy'  
+     };  
+       
+     console.log(example['pizza']);  
+   
+  // The above code will print the string 'yummy' to the terminal.  
